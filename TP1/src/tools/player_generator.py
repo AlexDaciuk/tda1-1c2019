76f997288f913e73c79d37011fd088179c403cb5
Alexis Daciuk <adaciuk@gmail.com>
2019-03-21 02:37:11 -0300

    Ahora genera el ranking de jugadores y las preferencias de cada jugador

--- TP1/src/tools/player_generator.py
+++ TP1/src/tools/player_generator.py
@@ -8,6 +8,9 @@ players = ["Juan", "Jose", "Ana", "Helena", "Martin", "Virginia", "Julia",
 numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
            "14", "15", "16", "17", "18", "19", "20"]
 
+numbers_mod = list(numbers)
+numbers_shuffled = list(numbers)
+
 file = open("../../assets/txt/20_jugadores.rank", "w+")
 
 # Ordeno el array alfabeticamente
@@ -15,13 +18,35 @@ players.sort()
 
 for name in players:
 
-    number_tmp = random.choice(numbers)
+    # Elijo un numero random, se va a usar para el ranking principal
+    number_tmp = random.choice(numbers_mod)
+
+    # Mezclo los numeros para crear las preferencias aleatorias
+    # Elimino el numero propio del jugador en cuestion, ya que nunca
+    # se podria elegir a si mismo
+    numbers_shuffled = list(numbers)
+    random.shuffle(numbers_shuffled)
+    numbers_shuffled.remove(number_tmp)
 
     pref_file_tmp = "jugador_" + number_tmp + ".pref"
 
+    # Abro el archivo
+    pref_file = open("../../assets/txt/" + pref_file_tmp, "w+")
+
+    for name2 in players:
+        if name2 != name:
+            pref_jug_tmp = name2 + "," + numbers_shuffled.pop() + "\n"
+            pref_file.write(pref_jug_tmp)
+
+    # Cierro el archivo de preferencias del jugador number_tmp
+    pref_file.close()
+
     player_string = number_tmp + "," + name + "," + pref_file_tmp + "\n"
 
-    numbers.remove(number_tmp)
+    print(numbers)
+    print(numbers_mod)
+
+    numbers_mod.remove(number_tmp)
 
     file.write(player_string)
 
