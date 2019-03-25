86523efdc34aefd2ea6dd6d19eaece0485e413b8
Alexis Daciuk <adaciuk@gmail.com>
2019-03-24 21:18:10 -0300

    Agrego matcher, por ahora leo los archivos y armo las listas de los mejores 10, peores 10, estoy teniendo problemas cargando la lista de preferencias de cada jugador, por alguna razon

---
+++ TP1/src/Primera_Parte/matcher.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+import sys
+
+file_path = "../../assets/txt/" + sys.argv[2]
+players_quantity = sys.argv[1]
+
+best_10 = []
+worse_10 = []
+matches = []
+
+
+def leer_archivo(file_path, players_quantity):
+    file = open(file_path, "r")
+
+    lines = file.readlines()
+
+    for line in lines:
+        splitted_line = line.split(',')
+
+        rank = int(splitted_line[0])
+        name = splitted_line[1]
+        pref_file = "../../assets/txt/" + splitted_line[2].rstrip()
+
+        player_tmp = Players(name, pref_file)
+
+        if rank <= 10:
+            best_10.append(player_tmp)
+        else:
+            worse_10.append(player_tmp)
+
+
+class Players:
+    def __init__(self, name, pref_file):
+        self.name = name
+        self.pref_list = []
+        self.load_pref(pref_file)
+
+    def load_pref(self, pref_file):
+        file = open(pref_file, "r")
+
+        pref_lines = file.readlines()
+
+        file.close()
+
+        for pref in pref_lines:
+            splitted_pref = pref.split(',')
+
+            name_tmp = splitted_pref[0]
+            pref_number = int(splitted_pref[1])
+
+            self.pref_list.insert(pref_number, name_tmp)
+
+
+def main():
+    leer_archivo(file_path, players_quantity)
+
+    for person in best_10:
+        print(person.name)
+        for pref in person.pref_list:
+            print(person.pref_list.index(pref) + 1, pref)
+
+    for person in worse_10:
+        print(person.name)
+        for pref in person.pref_list:
+            print(person.pref_list.index(pref) + 1, pref)
+
+
+main()
