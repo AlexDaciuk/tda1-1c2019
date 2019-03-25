3f7ca65e76844924ba4c48adf100e4940be11270
Alexis Daciuk <adaciuk@gmail.com>
2019-03-24 21:54:09 -0300

    Arreglo un poco de la parte de generacion de archivos, antes, las preferencias personales, iban del 1 al 20, salteandose el numero correspondiente al ranking general del jugador en cuestion, arregle eso

--- TP1/src/tools/player_generator.py
+++ TP1/src/tools/player_generator.py
@@ -7,11 +7,9 @@ players = ["Juan", "Jose", "Ana", "Helena", "Martin", "Virginia", "Julia",
            "Matias", "Luis", "Veronica", "Daniel", "Lucia", "Lucas", "Marina",
            "Ariel", "Valeria", "Ezequiel", "Florencia", "Damian", "Romina"]
 
-numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13",
-           "14", "15", "16", "17", "18", "19", "20"]
+numbers = list(range(1, 21, 1))
 
 players_mod = list(players)
-numbers_shuffled = list(numbers)
 
 file = open("../../assets/txt/20_jugadores.rank", "w+")
 
@@ -26,24 +24,24 @@ for number_tmp in numbers:
     # Mezclo los numeros para crear las preferencias aleatorias
     # Elimino el numero propio del jugador en cuestion, ya que nunca
     # se podria elegir a si mismo
-    numbers_shuffled = list(numbers)
+    numbers_shuffled = list(range(1, 20, 1))
     random.shuffle(numbers_shuffled)
-    numbers_shuffled.remove(number_tmp)
 
-    pref_file_tmp = "jugador_" + number_tmp + ".pref"
+    pref_file_tmp = "jugador_" + str(number_tmp) + ".pref"
 
     # Abro el archivo
     pref_file = open("../../assets/txt/" + pref_file_tmp, "w+")
 
     for name2 in players:
         if name2 != player_tmp:
-            pref_jug_tmp = name2 + "," + numbers_shuffled.pop() + "\n"
+            pref_jug_tmp = name2 + "," + str(numbers_shuffled.pop()) + "\n"
             pref_file.write(pref_jug_tmp)
 
     # Cierro el archivo de preferencias del jugador number_tmp
     pref_file.close()
 
-    player_string = number_tmp + "," + player_tmp + "," + pref_file_tmp + "\n"
+    player_string = str(number_tmp) + "," + player_tmp + \
+        "," + pref_file_tmp + "\n"
 
     players_mod.remove(player_tmp)
 
