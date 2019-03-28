677ad74da6b497be90ddb75112aaabd4ae39d54f
Alexis Daciuk <adaciuk@gmail.com>
2019-03-28 14:07:02 -0300

    Agrego permutaciones

--- TP1/src/Segunda_Parte/implementacion_lista.py
+++ TP1/src/Segunda_Parte/implementacion_lista.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import sys
+import math
 
 file_path = sys.argv[1]
 
@@ -39,9 +40,7 @@ def moda(number_list):
     number_list.sort()
 
     if (len(number_list) % 2) == 0:  # Largo de lista par
-
-
-
+        None
 
 
 def mediana(number_list):
@@ -49,16 +48,43 @@ def mediana(number_list):
 
 
 def desviacion_estandar(number_list):
-    None
+    media_tmp = media(number_list)
+    suma_distancias = 0
+
+    for number in number_list:
+        distancia_media = number - media_tmp
+        suma_distancias = + distancia_media
+
+    media_de_suma = suma_distancias / len(number_list)
+
+    return math.sqrt(media_de_suma)
 
 
 def permutaciones(number_list):
+    def swap(n1, n2):
+        tmp = number_list[n1]
+        number_list[n1] = number_list[n2]
+        number_list[n2] = tmp
+
+    def generar_permutaciones(k, lista):
+        if k == 1:
+            print(lista)
+        else:
+            generar_permutaciones(k - 1, lista)
+
+        for i in range(1, k):
+            if (i % 2) == 0:
+                swap(lista[i], lista[k])
+            else:
+                swap(lista[1], lista[k])
+
+        print(lista)
+        generar_permutaciones(k - 1, lista)
+
+
+def variaciones_r_elementos_sin_repeticion(number_list, r):
     None
 
 
-def variaciones_r_elementos(number_list):
-    None
-
-
-def variaciones_r_elementos_repeticion(number_list):
+def variaciones_r_elementos(number_list, r):
     None
