790dff015757cf890c1af9c0d3438720a0ca5908
Alex Daciuk <adaciuk@gmail.com>
2019-04-01 18:31:51 -0300

    Arreglo la generacion de los archivos

--- TP1/src/tools/player_generator.py
+++ TP1/src/tools/player_generator.py
@@ -16,10 +16,17 @@ file = open("../../assets/txt/20_jugadores.rank", "w+")
 # Ordeno el array alfabeticamente
 players.sort()
 
-for number_tmp in numbers:
+best_10 = [None] * 10
+worse_10 = [None] * 10
 
+for number_tmp in numbers:
     # Elijo un nombre random, se va a usar para el ranking principal
     player_tmp = random.choice(players_mod)
+
+    if number_tmp < 11:
+        best_10[number_tmp - 1] = player_tmp
+    else:
+        worse_10[number_tmp - 11] = player_tmp
 
     # Mezclo los numeros para crear las preferencias aleatorias
     # Elimino el numero propio del jugador en cuestion, ya que nunca
@@ -29,17 +36,6 @@ for number_tmp in numbers:
 
     pref_file_tmp = "jugador_" + str(number_tmp) + ".pref"
 
-    # Abro el archivo
-    pref_file = open("../../assets/txt/" + pref_file_tmp, "w+")
-
-    for name2 in players:
-        if name2 != player_tmp:
-            pref_jug_tmp = name2 + "," + str(numbers_shuffled.pop()) + "\n"
-            pref_file.write(pref_jug_tmp)
-
-    # Cierro el archivo de preferencias del jugador number_tmp
-    pref_file.close()
-
     player_string = str(number_tmp) + "," + player_tmp + \
         "," + pref_file_tmp + "\n"
 
@@ -48,3 +44,39 @@ for number_tmp in numbers:
     file.write(player_string)
 
 file.close()
+
+worse_10_copia = list(worse_10)
+worse_10_copia.sort()
+
+for player in best_10:
+    pref_file_tmp = "jugador_" + str(best_10.index(player)) + ".pref"
+
+    # Abro el archivo
+    pref_file = open("../../assets/txt/" + pref_file_tmp, "w+")
+
+    numbers_10 = list(range(1, 11, 1))
+    random.shuffle(numbers_10)
+
+    for player_w in worse_10_copia:
+        pref_jug_tmp = player_w + "," + str(numbers_10.pop()) + "\n"
+        pref_file.write(pref_jug_tmp)
+
+    pref_file.close()
+
+best_10_copia = list(best_10)
+best_10_copia.sort()
+
+for player in worse_10:
+    pref_file_tmp = "jugador_" + str(worse_10.index(player)) + ".pref"
+
+    # Abro el archivo
+    pref_file = open("../../assets/txt/" + pref_file_tmp, "w+")
+
+    numbers_10 = list(range(1, 11, 1))
+    random.shuffle(numbers_10)
+
+    for player_w in best_10_copia:
+        pref_jug_tmp = player_w + "," + str(numbers_10.pop()) + "\n"
+        pref_file.write(pref_jug_tmp)
+
+    pref_file.close()
