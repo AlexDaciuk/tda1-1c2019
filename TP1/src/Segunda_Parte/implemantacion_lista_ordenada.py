ba40bf4e26b0fcde20680919aea85f758ff5dd1f
Juan <juanopizzi@gmail.com>
2019-04-06 20:37:02 -0300

    Mas cambios en la lista ordenada y el heap

--- TP1/src/Segunda_Parte/implemantacion_lista_ordenada.py
+++ TP1/src/Segunda_Parte/implemantacion_lista_ordenada.py
@@ -8,9 +8,22 @@ file_path = sys.argv[1]
 lista = []
 lista.sorted(reverse = True)
 
+def cargar_numeros(file_path):
+    file = open(file_path, "r")
+
+    lines = file.readline()
+
+    for line in lines:
+         number_tmp = line.rstrip()
+         lista.append(number_tmp)
+         lista.sorted(reverse = True)
+
 #Como es un heap de maximo es tan facil como devolver el primer elemento si este existe
-def maximo(lista:
+def maximo(lista):
      maxi = None 
      if (len(lista) != 0):
          maxi = lista[0]
      return maxi
+
+def mediana(lista):
+     return None
--- TP1/src/Segunda_Parte/implementacion_heap.py
+++ TP1/src/Segunda_Parte/implementacion_heap.py
@@ -9,6 +9,15 @@ lista = []
 heap = heapq.heapify(lista) #Ahora la lista es un heap de minimo
 maxHeap = heapq._heapify_max(heap) #Ahora es un heap de maximo 
 
+def cargar_numeros(file_path):
+    file = open(file_path, "r")
+
+    lines = file.readline()
+
+    for line in lines:
+        number_tmp = line.rstrip()
+         heapq.heappush(maxHeap,MaxHeapInt(number_tmp))  #Creo que asi se tiene que insertar para respetar que es un heap de maximo
+
 #Como es un heap de maximo es tan facil como devolver el primer elemento si este existe
 def maximo(maxHeap):
      maxi = None 
