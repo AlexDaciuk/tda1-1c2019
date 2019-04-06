717aac2ecd08826e69cb0c2c6f6ab7165c798210
Juan <juanopizzi@gmail.com>
2019-04-06 20:05:20 -0300

    Agrego un par de cosas al achivo de la lista ordenada

---
+++ TP1/src/Segunda_Parte/implemantacion_lista_ordenada.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+import sys
+import math
+import heapq
+
+file_path = sys.argv[1]
+
+lista = []
+lista.sorted(reverse = True)
+
+#Como es un heap de maximo es tan facil como devolver el primer elemento si este existe
+def maximo(lista:
+     maxi = None 
+     if (len(lista) != 0):
+         maxi = lista[0]
+     return maxi
