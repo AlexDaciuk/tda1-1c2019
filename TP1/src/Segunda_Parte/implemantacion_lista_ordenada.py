d983dde491b4fb6beeb5253500a9506087ae0bd7
Alex Daciuk <adaciuk@gmail.com>
2019-04-09 01:06:05 -0300

    Arreglo probelmas de formato e identacion, elimino imports que no se usan

--- TP1/src/Segunda_Parte/implemantacion_lista_ordenada.py
+++ TP1/src/Segunda_Parte/implemantacion_lista_ordenada.py
@@ -1,12 +1,11 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 import sys
-import math
-import heapq
 
 file_path = sys.argv[1]
 
 lista = []
-lista.sorted(reverse = True)
+lista.sorted(reverse=True)
+
 
 def cargar_numeros(file_path):
     file = open(file_path, "r")
@@ -14,24 +13,29 @@ def cargar_numeros(file_path):
     lines = file.readline()
 
     for line in lines:
-         number_tmp = line.rstrip()
-         lista.append(number_tmp)
-         lista.sorted(reverse = True)
+        number_tmp = line.rstrip()
+        lista.append(number_tmp)
+        lista.sorted(reverse=True)
 
-#Como es un heap de maximo es tan facil como devolver el primer elemento si este existe
+# Como es un heap de maximo es tan facil como devolver el primer elemento
+# si este existe
+
+
 def maximo(lista):
-     maxi = None 
-     if (len(lista) != 0):
-         maxi = lista[0]
-     return maxi
+    maxi = None
+    if (len(lista) != 0):
+        maxi = lista[0]
+    return maxi
 
 # No veo por que esta funcion deberia ser distinta a la de la lista desordenada
 # Salvo en el precondicion de que aca la lista ya llega ordenada
+
+
 def mediana(lista):
-      pos = (len(lista) - 1) // 2
-     # Si el largo de la lista es par devuelvo el promedio 
-     # de los 2 valores medios de la lista
-     if (len(lista) % 2) == 0:
-         return (lista[pos] + lista[pos + 1]) / 2.0
-     # Sino devuelvo el valor medio
-     return lista[pos]
+    pos = (len(lista) - 1) // 2
+    # Si el largo de la lista es par devuelvo el promedio
+    # de los 2 valores medios de la lista
+    if (len(lista) % 2) == 0:
+        return (lista[pos] + lista[pos + 1]) / 2.0
+    # Sino devuelvo el valor medio
+    return lista[pos]
