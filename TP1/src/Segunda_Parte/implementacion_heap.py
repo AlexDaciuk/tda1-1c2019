f05a8cd642ef52c21b03e93074c7ac483af0d5ac
Alex Daciuk <adaciuk@gmail.com>
2019-04-09 01:11:24 -0300

    Arreglo horrores de formateo

--- TP1/src/Segunda_Parte/implementacion_heap.py
+++ TP1/src/Segunda_Parte/implementacion_heap.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 import sys
-import math
 import heapq
 
 file_path = sys.argv[1]
 
 lista = []
-heap = heapq.heapify(lista) #Ahora la lista es un heap de minimo
-maxHeap = heapq._heapify_max(heap) #Ahora es un heap de maximo 
+heap = heapq.heapify(lista)  # Ahora la lista es un heap de minimo
+maxHeap = heapq._heapify_max(heap)  # Ahora es un heap de maximo
+
 
 def cargar_numeros(file_path):
     file = open(file_path, "r")
@@ -16,13 +16,16 @@ def cargar_numeros(file_path):
 
     for line in lines:
         number_tmp = line.rstrip()
-         heapq.heappush(maxHeap,MaxHeapInt(number_tmp))  #Creo que asi se tiene que insertar para respetar que es un heap de maximo
+        # Creo que asi se tiene que insertar para respetar que es un heap
+        # de maximo
+        heapq.heappush(maxHeap, MaxHeapInt(number_tmp))
 
-#Como es un heap de maximo es tan facil como devolver el primer elemento si este existe
+# Como es un heap de maximo es tan facil como devolver el primer elemento si
+# este existe
+
+
 def maximo(maxHeap):
-     maxi = None 
-     if (len(maxHeap) != 0):
-         maxi = maxHeap[0].val
-     return maxi
-
-
+    maxi = None
+    if (len(maxHeap) != 0):
+        maxi = maxHeap[0].val
+    return maxi
