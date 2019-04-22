f3164ec879ef2707a661da53e93c5e164b2d71f3
Alexis Daciuk <adaciuk@gmail.com>
2019-04-22 14:47:39 -0300

    Creo el mapa vacio y le pongo paredes a los costados

---
+++ TP2/src/Primera_Parte/generar_laberinto.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+import sys
+
+# El orden de los argumentos esta definido en el enunciado
+metodo = sys.argv[1]
+
+alto = int(sys.argv[2])
+ancho = int(sys.argv[3])
+
+
+def crear_mapa_vacio(alto, ancho):
+    # Creo el mapa vacio
+    mapa_tmp = [[" " for x in range(ancho)] for y in range(alto)]
+
+    poner_paredes(mapa_tmp, alto, ancho)
+
+    return mapa_tmp
+
+
+def poner_paredes(mapa, alto, ancho):
+    for i in range(1, ancho - 1):
+        # Pared superior e inferior, uso signo menos
+        mapa[0][i] = "¯"
+        mapa[alto - 1][i] = "_"
+    for i in range(alto):
+        # Paredes laterales, uso pipe
+        mapa[i][0] = "|"
+        mapa[i][ancho - 1] = "|"
+
+
+def mostrar_mapa(mapa):
+    for fila in range(len(mapa)):
+        print(*mapa[fila], sep=' ')
+
+
+if __name__ == "__main__":
+    mapa = crear_mapa_vacio(alto, ancho)
+
+    mostrar_mapa(mapa)
