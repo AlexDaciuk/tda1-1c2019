069f11df0f3da15a1038242f11df4aee61ce937b
Alex Daciuk <adaciuk@gmail.com>
2019-04-28 15:31:43 -0300

    Subo lo que venia haciendo hasta ahora, ya tengo una funcion que crea paredes en cualquier orientacion y de cualquier largo y automaticamente le saca un bloque para generar un pasaje

--- TP2/src/Primera_Parte/generar_laberinto.py
+++ TP2/src/Primera_Parte/generar_laberinto.py
@@ -1,31 +1,33 @@
 #!/usr/bin/env python3
 import sys
+import random
 
 # El orden de los argumentos esta definido en el enunciado
 metodo = sys.argv[1]
 
-alto = int(sys.argv[2])
-ancho = int(sys.argv[3])
+alto = int(sys.argv[2]) + 2
+ancho = int(sys.argv[3]) + 2
 
 
 def crear_mapa_vacio(alto, ancho):
-    # Creo el mapa vacio
-    mapa_tmp = [[" " for x in range(ancho + 2)] for y in range(alto + 2)]
+    # Creo el mapa vacio, sumo 2 a alto y ancho, asi el mapa queda
+    # del tamaño que me piden y las paredes exteriores no le sacan tamaño
+    mapa_tmp = [[" " for x in range(ancho)] for y in range(alto)]
 
-    poner_paredes(mapa_tmp, alto, ancho)
+    poner_paredes_externas(mapa_tmp, alto, ancho)
 
     return mapa_tmp
 
 
-def poner_paredes(mapa, alto, ancho):
-    for i in range(1, ancho + 1):
+def poner_paredes_externas(mapa, alto, ancho):
+    for i in range(1, ancho - 1):
         # Pared superior e inferior, uso signo menos
-        mapa[0][i] = "¯"
-        mapa[alto + 1][i] = "_"
-    for i in range(alto + 2):
+        mapa[0][i] = "*"
+        mapa[alto + 1][i] = "*"
+    for i in range(alto - 2):
         # Paredes laterales, uso pipe
-        mapa[i][0] = "|"
-        mapa[i][ancho + 1] = "|"
+        mapa[i][0] = "*"
+        mapa[i][ancho + 1] = "*"
 
 
 def mostrar_mapa(mapa):
@@ -33,8 +35,36 @@ def mostrar_mapa(mapa):
         print(*mapa[fila], sep=' ')
 
 
+def poner_pared(mapa, orientacion, posicion, desde, hasta):
+    pos_random = random.randint(desde + 1, hasta - 2)
+
+    if orientacion == "horizontal":
+        for celda in range(desde, hasta):
+            mapa[posicion][celda] = "*"
+        mapa[posicion][pos_random] = " "
+
+    elif orientacion == "vertical":
+        for celda in range(desde, hasta):
+            mapa[celda][posicion] = "*"
+        mapa[pos_random][posicion] = " "
+
+
+def decidir_orientacion(alto, ancho):
+    if alto > ancho:
+        return "horizontal"
+    elif ancho < alto:
+        return "vertical"
+    else:
+        return (random.choice(["horizontal", "vertical"]))
+
+
 def metodo_dyc(mapa):
-    None
+    alto_tmp = random.randint(2, alto - 2)
+    ancho_tmp = random.randint(2, ancho - 2)
+    # Pongo la primera pared para arrancar
+    orientacion_tmp = decidir_orientacion(alto_tmp, ancho_tmp)
+
+    poner_pared(mapa, orientacion_tmp, ancho_tmp, 0, )
 
 
 def metodo_dfs(mapa):
@@ -43,7 +73,6 @@ def metodo_dfs(mapa):
 
 if __name__ == "__main__":
     mapa = crear_mapa_vacio(alto, ancho)
-
     mostrar_mapa(mapa)
 
     if metodo == "dyc":
