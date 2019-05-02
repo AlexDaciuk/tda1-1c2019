4617346498c5d68ded6c126ec26cbbe76897f84f
Alexis Daciuk <adaciuk@gmail.com>
2019-05-01 21:31:18 -0300

    Empiezo la segunda parte del TP2

---
+++ TP2/src/Segunta_Parte/sospechosos.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+import sys
+
+file_path = sys.argv[1]
+
+planilla = []
+
+
+def cargar_planilla(file_path):
+    file = open(file_path, "r")
+
+    lines = file.readlines()
+
+    for line in lines:
+        persona_tmp = line.split(",")
+        persona_tmp[1] = int(persona_tmp[1])
+        persona_tmp[2] = int(persona_tmp[2].rstrip())
+
+        planilla.append(persona_tmp)
+
+
+if __name__ == "__main__":
+    cargar_planilla(file_path)
+
+    print(planilla)
