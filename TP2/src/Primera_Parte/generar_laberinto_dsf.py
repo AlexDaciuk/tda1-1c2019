11fd1f57c727283fcf3528a55362e43f5dbfd20a
Matias Onorato <onoratomatias@gmail.com>
2019-05-04 20:02:46 -0300

    parte 1 - generación dfs completada

--- TP2/src/Primera_Parte/generar_laberinto_dsf.py
+++ TP2/src/Primera_Parte/generar_laberinto_dsf.py
@@ -1,5 +1,8 @@
+import sys
+from pathlib import Path
 from random import randint
 from tkinter import Tk, Canvas, mainloop
+
 
 #clase que funciona de nodo y contiene las aristas que lo conectan
 class Celda:
@@ -15,9 +18,11 @@ class Laberinto:
          self.cols = cols
          # Grafo del laberinto con diccionario de adyacencias y representación matricial de los nodos (celdas)
          self.grilla = [[Celda(x,y) for y in range(self.fils)] for x in range(self.cols)] 
+         self.pila = [self.grilla[0][0]]
+         self.generar(self.grilla[0][0])
 
-    # Busca un vecino al azar, lo conecta con la celda y lo marca como visitado
-    def obtenerVecino(self, celda):
+    # Busca un vecino al azar, lo conecta con la celda y lo retorna. Si no existe retorna None
+    def conectarVecino(self, celda):
         vecinos = []
 
         # Busco vecinos no visitados
@@ -31,7 +36,7 @@ class Laberinto:
             vecinos.append({'pos':'O', 'celda':  self.grilla[celda.x-1][celda.y]})
 
         # Eligo uno al azar
-        if len(vecinos) > 0:        
+        if vecinos:  
             vecino = vecinos[randint(0,len(vecinos)-1)]
         else:
             return None
@@ -48,12 +53,38 @@ class Laberinto:
         if pos == 'E': return 'O'
         if pos == 'O': return 'E'
     
+    #dfs con recursive backtraking
+    def generar(self, celda):                
+        if self.pila[-1] is not None:
+            celda.visitado = True
+            self.pila.append(self.conectarVecino(celda))
+            self.generar(self.pila[-1])
+        else:
+            try:
+                self.pila.pop()
+                self.pila.pop() 
+                self.generar(self.pila[-1])              
+            except IndexError:
+                return
+
+    def generar2(self,celda):                
+        if type(celda) is Celda :
+            celda.visitado = True
+            if self.conectarVecino(celda) is not None:
+                self.pila.append(celda)            
+            self.generar2(self.conectarVecino(celda))
+        elif celda is None and self.pila:
+            self.pila.pop()
+            self.generar2(self.pila[-1])
+        else:
+            return
+                 
 class Graficador:
-    def __init__(self,laberinto):
+    def __init__(self,laberinto, wcelda):
         self.laberinto = laberinto
         self.master = Tk()
-        self.w = 40 # Ancho de celda
-        self.canva = Canvas(self.master, width=(self.laberinto.fils*self.w)+2, height=(self.laberinto.cols*self.w)+2)
+        self.w = wcelda # Ancho de celda
+        self.canva = Canvas(self.master, width=(self.laberinto.cols*self.w)+2, height=(self.laberinto.fils*self.w)+2)
 
     def imprimir(self):
         # Imprimo celdas        
@@ -76,17 +107,16 @@ class Graficador:
         if celda.conexiones['S'] is None:
             self.canva.create_line(ix+2,fy+2,fx+2,fy+2, fill="black") # Sur
         if celda.conexiones['O'] is None:
-            self.canva.create_line(ix+2,iy+2,ix+2,fy+2, fill="black") # Oeste         
+            self.canva.create_line(ix+2,iy+2,ix+2,fy+2, fill="black") # Oeste     
+
+    def guardar(self):
+        pass
+
+        archivo =     
 
 if __name__ == "__main__": 
+    sys.setrecursionlimit(5000)
     lab = Laberinto(10,10)
-    graf = Graficador(lab)
-
-    # algoritmo por dsf
-    a = [lab.grilla[0][0]]
-    while(a[-1] is not None):
-        a[-1].visitado=True          
-        a.append(lab.obtenerVecino(a[-1]))
-
+    graf = Graficador(lab,30)
     graf.imprimir()
     mainloop()
