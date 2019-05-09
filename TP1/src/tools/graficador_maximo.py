5104258bcee4272343bd9543cff772fab784f36e
Alexis Daciuk <adaciuk@gmail.com>
2019-05-09 19:41:25 -0300

    Agrego el codigo que grafica la funcion maximo

---
+++ TP1/src/tools/graficador_maximo.py
@@ -0,0 +1,57 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Setteo titulo del grafico
+plt.title("Maximo")
+
+# Indico cantidad de benchmarks y nombres
+datapoints_number = 10
+datapoints_names = ["1",
+                    "10",
+                    "100",
+                    "1000",
+                    "10 000",
+                    "100 000",
+                    "1 000 000",
+                    "10 000 000"]
+
+# Armo listas con los tiempos de ejecucion de cada implementacion
+tiempos_lista = [1.40E-6, 1.81E-6, 3.76E-6,
+                 2.32E-5, 0.0002, 0.002, 0.020, 0.200]
+tiempos_lista_ordenada = []
+tiempos_arbol_abb = []
+
+# Calculo la cantidad de posiciones en el eje x
+index = np.arange(len(datapoints_names))
+
+# Setteo las variables del grafico
+bar_width = 0.2
+opacity = 0.5
+
+# Armo las barras de cada implementacion
+barra_lista = plt.bar(index, tiempos_lista, bar_width,
+                      alpha=0.7,
+                      color="c",
+                      label="Lista / Vector")
+
+barra_lista_ordenada = plt.bar(index + bar_width, tiempos_lista_ordenada,
+                               bar_width,
+                               alpha=0.7,
+                               color="b",
+                               label="Lista / Vector\n ordenado")
+
+barra_arbol_abb = plt.bar(index + 2 * bar_width, tiempos_arbol_abb,
+                          bar_width,
+                          alpha=0.7,
+                          color="m",
+                          label="Arbol ABB")
+
+plt.grid(True, axis='y', linewidth=0.2)
+plt.xlabel('Data points')
+plt.ylabel('Tiempo (segundos)')
+plt.xticks(index + 2 * bar_width, datapoints_names)
+plt.yticks(np.arange(0, 1, 0.001))
+plt.legend()
+
+plt.tight_layout()
+plt.savefig("../../assets/images/maximo.png")
