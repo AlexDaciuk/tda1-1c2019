f02590458fd2ec873b64c6b4ccef8971fc830a17
Alexis Daciuk <adaciuk@gmail.com>
2019-05-12 16:01:34 -0300

    Arreglo lo que habia armado antes, era un grafico de barra cuando necesitabamos un grafico de lineas

--- TP1/src/tools/graficador_maximo.py
+++ TP1/src/tools/graficador_maximo.py
@@ -1,8 +1,11 @@
 import matplotlib.pyplot as plt
-import numpy as np
 
 # Setteo titulo del grafico
 plt.title("Maximo")
+plt.style.use('seaborn-whitegrid')
+
+plt.xlabel("Tamaño de vector.")
+plt.ylabel("Segundos")
 
 # Indico cantidad de benchmarks y nombres
 datapoints_number = 10
@@ -21,37 +24,9 @@ tiempos_lista = [1.40E-6, 1.81E-6, 3.76E-6,
 tiempos_lista_ordenada = []
 tiempos_arbol_abb = []
 
-# Calculo la cantidad de posiciones en el eje x
-index = np.arange(len(datapoints_names))
-
-# Setteo las variables del grafico
-bar_width = 0.2
-opacity = 0.5
-
-# Armo las barras de cada implementacion
-barra_lista = plt.bar(index, tiempos_lista, bar_width,
-                      alpha=0.7,
-                      color="c",
-                      label="Lista / Vector")
-
-barra_lista_ordenada = plt.bar(index + bar_width, tiempos_lista_ordenada,
-                               bar_width,
-                               alpha=0.7,
-                               color="b",
-                               label="Lista / Vector\n ordenado")
-
-barra_arbol_abb = plt.bar(index + 2 * bar_width, tiempos_arbol_abb,
-                          bar_width,
-                          alpha=0.7,
-                          color="m",
-                          label="Arbol ABB")
-
-plt.grid(True, axis="y", linewidth=0.2)
-plt.xlabel("Cantidad de Elementos")
-plt.ylabel("Tiempo (segundos)")
-plt.xticks(index + 2 * bar_width, datapoints_names)
-plt.yticks(np.arange(0, 1, 0.001))
-plt.legend()
+plt.plot(datapoints_names, tiempos_lista, '--g')
+plt.plot(datapoints_names, tiempos_lista_ordenada, ':r')
+plt.plot(datapoints_names, tiempos_arbol_abb, '-.k')
 
 plt.tight_layout()
 plt.savefig("../../assets/images/maximo.png")
