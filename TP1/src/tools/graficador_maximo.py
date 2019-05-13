0d1c4e0575ddeed832b1166f551c34be800e79b7
Alexis Daciuk <adaciuk@gmail.com>
2019-05-13 15:37:57 -0300

    Agrego datos y labels

--- TP1/src/tools/graficador_maximo.py
+++ TP1/src/tools/graficador_maximo.py
@@ -19,12 +19,16 @@ datapoints_names = ["1",
 # Armo listas con los tiempos de ejecucion de cada implementacion
 tiempos_lista = [1.40E-6, 1.81E-6, 3.76E-6,
                  2.32E-5, 0.0002, 0.002, 0.020, 0.200]
-tiempos_lista_ordenada = []
-tiempos_arbol_abb = []
+tiempos_lista_ordenada = [1.54E-6, 1.50E-6, 1.78E-6,
+                          1.71E-6, 3.74E-6, 5.53E-6, 5.53E-6, 5.92E-6]
+tiempos_arbol_abb = [1.57E-6, 1.62E-6, 1.85E-6,
+                     3.17E-6, 4.98E-6, 9.22E-6, 1.05E-5, 1.28E-5]
 
-plt.plot(datapoints_names, tiempos_lista, '--g')
-plt.plot(datapoints_names, tiempos_lista_ordenada, ':r')
-plt.plot(datapoints_names, tiempos_arbol_abb, '-.k')
+plt.plot(datapoints_names, tiempos_lista, 'g', label="Lista / Vector")
+plt.plot(datapoints_names, tiempos_lista_ordenada,
+         ':r', label="Lista / Vector ordenado")
+plt.plot(datapoints_names, tiempos_arbol_abb, '-.k', label="Arbol ABB")
+plt.legend()
 
 plt.tight_layout()
 plt.savefig("../../assets/images/maximo.png")
--- TP1/src/tools/graficador_media.py
+++ TP1/src/tools/graficador_media.py
@@ -19,13 +19,19 @@ datapoints_names = ["1",
                     "10 000 000"]
 
 # Armo listas con los tiempos de ejecucion de cada implementacion
-tiempos_lista = []
-tiempos_lista_ordenada = []
-tiempos_arbol_abb = []
+tiempos_lista = [1.88E-6, 2.38E-6, 5.88E-6,
+                 3.19E-6, 0.0003, 0.0032, 0.033, 0.334]
+tiempos_lista_ordenada = [2.31E-6, 3.29E-6,
+                          6.03E-6, 3.69E-5, 0.0002, 0.006, 0.100, 1.31]
+tiempos_arbol_abb = [4.55E-6, 1.13E-5, 6.48E-5,
+                     0.00064, 0.0079, 0.114, 1.20, 13.28]
 
-plt.plot(datapoints_names, tiempos_lista, '--g')
-plt.plot(datapoints_names, tiempos_lista_ordenada, ':r')
-plt.plot(datapoints_names, tiempos_arbol_abb, '-.k')
+plt.plot(datapoints_names, tiempos_lista, '--g', label="Lista / Vector")
+plt.plot(datapoints_names, tiempos_lista_ordenada,
+         ':r', label="Lista / Vector Ordenado")
+plt.plot(datapoints_names, tiempos_arbol_abb, '-.k', label="Arbol ABB")
+plt.legend()
+
 
 plt.tight_layout()
 plt.savefig("../../assets/images/media.png")
