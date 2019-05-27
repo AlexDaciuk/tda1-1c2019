46ba2ad8e23fc9d9def46b23b406eb59c40dd64c
Alexis Daciuk <adaciuk@gmail.com>
2019-05-27 04:51:13 -0300

    Reescrito de 0 con objetos, casi todos los casos que subieron los profes funcionan menos el caso 1 que es el mas complicado

--- TP2/src/Segunta_Parte/sospechosos.py
+++ TP2/src/Segunta_Parte/sospechosos.py
@@ -1,81 +1,143 @@
 #!/usr/bin/env python3
 import sys
 from pathlib import Path
+import copy
 
 file_path = sys.argv[1]
 
 planilla = []
 
 
-def cargar_planilla(file_path):
-    file = open(file_path, "r")
+class Entrada:
+    def __init__(self, line):
+        entrada_tmp = line.split(",")
+
+        self.nombre = entrada_tmp[0]
+        self.horario_entrada = int(entrada_tmp[1])
+        self.tiempo_estadia = int(entrada_tmp[2].rstrip())
+        self.horario_salida = self.horario_entrada + self.tiempo_estadia
+
+    def mostrar(self):
+        print(
+            self.nombre + ","
+            + str(self.horario_entrada)
+            + "," + str(self.tiempo_estadia)
+            + "," + str(self.horario_salida)
+        )
+
+
+def cargar_planilla(file):
+    datos = []
+
+    file = open(Path(file_path), "r")
 
     lines = file.readlines()
 
     for line in lines:
-        persona_tmp = line.split(",")
-        persona_tmp[1] = int(persona_tmp[1])
-        persona_tmp[2] = int(persona_tmp[2].rstrip())
+        if int(line.split(",")[2]) <= 120:
+            datos.append(Entrada(line))
 
-        planilla.append(persona_tmp)
+    return datos
 
 
-def validar_hora(hora_sospechoso, hora_lista):
-    diff_sospechoso_min = hora_sospechoso - hora_lista
+class ListaSospechosos:
+    def __init__(self, primer_sospechoso):
+        self.lista_sospechosos = []
+        self.primer_entrada = primer_sospechoso.horario_entrada
+        self.lista_sospechosos.append(primer_sospechoso)
+        self.primer_salida = primer_sospechoso.horario_salida
+        self.ultima_salida = primer_sospechoso.horario_salida
 
-    if int(diff_sospechoso_min) < 120:
+    def agregar_sospechoso(self, sospechoso):
+        if sospechoso not in self.lista_sospechosos:
+            self.lista_sospechosos.append(sospechoso)
+
+        if sospechoso.horario_salida < self.primer_salida:
+            self.primero_salir = sospechoso.horario_salida
+        elif sospechoso.horario_salida > self.ultima_salida:
+            self.ultima_salida = sospechoso.horario_salida
+
+    def califica(self, sospechoso):
+        # Chequeo que haya entrado en los 120 minutos de la ventana de la lista
+        if sospechoso.horario_entrada - self.primer_entrada > 120:
+            return False
+
+        # Chequeo que el que voy a agregar, estuvo en algun momento con todo el
+        # grupo de sospechosos
+        # Caso 1 : entrada posterior a X persona de la lista, la entrada del
+        # nuevo sospechoso tiene que ser anterior a la hora de salida de X
+        # Caso 2 : la entrada es a la misma hora que la del sospechoso X,
+        # cumple, ya que no existen estadias de 0 minutos
+        for actual_sospechoso in self.lista_sospechosos:
+            if actual_sospechoso.horario_salida < sospechoso.horario_entrada:
+                return False
+
         return True
 
+    def largo(self):
+        return len(self.lista_sospechosos)
 
-def buscar_sospechosos(planilla):
-    lista_tmp = []
-    sospechosos = []
+    def mostrar(self):
+        print("Lista de sospechosos : ")
+        for sospechoso in self.lista_sospechosos:
+            print(sospechoso.nombre)
 
-    planilla_copia = list(planilla)
-
-    # Agrego la primera persona que entro como sospechosa
-    # asi como la hora de ingreso, para que sea mas facil chequear que esta
-    # en el timeframe
-    # Formato [hora_primer_soospechoso, sospechoso_1 , ... , sospechoso_n]
-
-    lista_tmp.append([planilla[0][1], planilla[0][0]])
-
-    planilla_copia.remove(planilla[0])
-
-    for persona in planilla_copia:
-        for lista in lista_tmp:
-            if validar_hora(persona[1], lista[0]):
-                lista.append(persona[0])
-            else:
-                lista_tmp.append([persona[0], persona[1]])
-
-    # for lista in lista_tmp:
-    #     lista = list(dict.fromkeys(lista))
-    #     sospechosos.append(lista)
-
-    return sospechosos
+    def tiempo_total(self):
+        return self.ultima_salida - self.primer_entrada
 
 
-def guardar_archivo(sospechosos):
-    file_path = "../../assets/txt/sospechosos.txt"
+class ArmadorListas:
+    def __init__(self, planilla):
+        self.planilla = planilla
+        self.listas = []
+        # Creo la primera listas trivial con la primera persona que ingresa
+        self.listas.append(ListaSospechosos(planilla[0]))
+        # Saco a esa persona asi no lo tomo 2 veces
+        self.planilla = self.planilla[1:]
 
-    file = open(Path(file_path), 'w+')
+    def largo_valido(self, lista):
+        if lista.largo() >= 5 and lista.largo() <= 10:
+            return True
+        else:
+            return False
 
-    file.write("Los diferentes grupos de sospechosos son : \n")
+    def tiempo_valido(self, lista):
+        if lista.tiempo_total() >= 40 and lista.tiempo_total() <= 120:
+            return True
+        else:
+            return False
 
-    for fila in range(len(sospechosos)):
-        linea_tmp = ", ".join(str(x) for x in sospechosos[fila]) + "\n"
-        file.write(linea_tmp)
+    def armar_listas(self):
+        for entrada in self.planilla:
+            for lista in self.listas:
+                if lista.califica(entrada):
+                    lista.agregar_sospechoso(entrada)
+                else:
+                    self.listas.append(ListaSospechosos(entrada))
 
-    file.close()
+        self.definitiva = []
+
+        for lista in self.listas:
+            # lista.mostrar()
+            # print("Mi tiempo total es de :" + str(lista.tiempo_total()))
+            # print(lista.horario_primer_entrada)
+            # print(lista.horario_ultimo_en_salir)
+            if self.largo_valido(lista) and self.tiempo_valido(lista):
+                # print("Entre")
+                self.definitiva.append(lista)
+
+        return self.definitiva
 
 
 if __name__ == "__main__":
-    cargar_planilla(file_path)
+    planilla = cargar_planilla(file_path)
 
-    print(planilla)
+    for entrada in planilla:
+        entrada.mostrar()
 
-    print("Las listas de sospechosos son : ")
-    sospechosos = buscar_sospechosos(planilla)
-    print(sospechosos)
-    guardar_archivo(sospechosos)
+    if len(planilla) > 0:
+        armador = ArmadorListas(planilla)
+        listas = armador.armar_listas()
+
+        for lista in listas:
+            lista.mostrar()
